de2925069c7c5f2327c92dd7e9a9ab7021cd448c
JoseAntonioCaballero <ja.caballero.mtz@gmail.com>
2019-06-12 02:14:02 -0500

    add newCompiler with objects

---
+++ Compiladores/codeExamples/analisisCompilador/CompilerFunctions.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <fstream>
+#include <stdlib.h>
+#include <list>
+#include <string.h>
+#include <sstream>
+#include <vector>
+
+using namespace std;
+class AnalisisLexico{
+	public:
+	ifstream in;
+	ofstream out;
+	ifstream palabrasReservadas;
+	list<string> listaPalabras;
+	
+	bool esReservada(char *id){//funcion es reservada busca en el arcchivo de palabras reservadas
+		bool reservada = false;
+		for(list<string>::iterator it=listaPalabras.begin();it!=listaPalabras.end();it++){
+			if(strcmp(id,(*it).c_str())==0){
+                reservada=true;
+                break;
+            }
+        }
+		return reservada;
+    }
+    
+    AnalisisLexico(char * entrada,char * salida){//constructor para inicializar la clase
+		getPalabrasReservadas();
+		
+	}
+	
+	private:
+	void getPalabrasReservadas(){//toma las palabras reservadas del archivo si es que existe.
+		palabrasReservadas.open("palabrasReservadas.txt");
+		try{
+			if (palabrasReservadas.fail()){
+				throw 1;
+			}else{
+				cout<<"exito al abrir el archivo plabras reservadas"<<endl;
+				string cadena;
+				while(palabrasReservadas.eof()==false){
+					palabrasReservadas>>cadena;
+					listaPalabras.push_back(cadena);
+				}
+			}
+		}catch(int error){
+			if (error==1){
+				cout<<"Error al abrir el archivo palabras reservadas"<<endl;
+			}else{
+				cout<<"ocurrio un error inesperado al querer abrir palabras reservadas."<<endl;
+			}
+		}
+	}
+    
+};
+int main(int nArgs, char ** args){
+	
+	return 0;	
+}
